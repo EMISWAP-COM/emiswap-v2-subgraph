90b9a8716b5fc65a74ce48a46c2df9361783a2cb
Nikita Bezhin <bezhin.nikita@gmail.com>
2021-09-06 19:00:04 +0300

    WBTC and KCC Network

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -114,6 +114,7 @@ export function findEthPerToken(token: Token, maxDepthReached: boolean): BigDeci
 let WHITELIST: string[] = [
   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
    ETH_ADDRESS, // ETH
+  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
   '0x0000000000004946c0e9f43f4dee607b0ef1fa1c', // CHI
   '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
